41bff00a38fe9c3a7594cc0771ee5942defcb678
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2024-04-07 15:58:07 +0800

    update

---
+++ build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/appallcom_2_qmltyperegistrations.cpp
@@ -0,0 +1,22 @@
+/****************************************************************************
+** Generated QML type registration code
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <QtQml/qqml.h>
+#include <QtQml/qqmlmoduleregistration.h>
+
+
+
+#if !defined(QT_STATIC)
+#define Q_QMLTYPE_EXPORT Q_DECL_EXPORT
+#else
+#define Q_QMLTYPE_EXPORT
+#endif
+Q_QMLTYPE_EXPORT void qml_register_types_allcom_2()
+{
+    qmlRegisterModule("allcom_2", 1, 0);
+}
+
+static const QQmlModuleRegistration allcom2Registration("allcom_2", qml_register_types_allcom_2);
